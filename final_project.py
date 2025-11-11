f7485c08aa3aea20901761716b1886b492ac418c
Akshatha912 <127100110+Akshatha912@users.noreply.github.com>
2025-11-11 18:59:24 +0530

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,110 +1,94 @@
 from datetime import datetime, timedelta
-import pendulum
-import os
-from airflow.decorators import dag
-from airflow.operators.dummy_operator import DummyOperator
-from final_project_operators.stage_redshift import StageToRedshiftOperator
-from final_project_operators.load_fact import LoadFactOperator
-from final_project_operators.load_dimension import LoadDimensionOperator
-from final_project_operators.data_quality import DataQualityOperator
-from udacity.common import final_project_sql_statements
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
 from airflow.operators.postgres_operator import PostgresOperator
+from airflow.utils.dates import days_ago
+
+from operators.stage_redshift import StageToRedshiftOperator
+from operators.load_fact import LoadFactOperator
+from operators.load_dimension import LoadDimensionOperator
+from operators.data_quality import DataQualityOperator
+from helpers.sql_queries import SqlQueries
 
 
-start_date = datetime(2018, 11, 1)
-end_date = datetime(2018, 11, 30)
-
-s3_bucket = "airflow-project-bucket123"
-events_s3_key = "log-data"
-songs_s3_key = "song-data/A/A/"
-log_json_file = 'log_json_path.json'
-
+# -------------------------------------------------------------------------
+# Default arguments for the DAG
+# -------------------------------------------------------------------------
 default_args = {
     'owner': 'udacity',
-    'start_date': pendulum.now(),
     'depends_on_past': False,
+    'start_date': days_ago(1),
     'retries': 3,
     'retry_delay': timedelta(minutes=5),
     'catchup': False,
     'email_on_retry': False
 }
 
-@dag(
+
+# -------------------------------------------------------------------------
+# Define the DAG
+# -------------------------------------------------------------------------
+dag = DAG(
+    'final_project_dag',
     default_args=default_args,
     description='Load and transform data in Redshift with Airflow',
-    schedule_interval='0 * * * *'
+    schedule_interval='@hourly',  # once per hour
+    max_active_runs=1,
+    tags=['udacity', 'sparkify', 'redshift']
 )
-def final_project():
 
-    start_operator = DummyOperator(task_id='Begin_execution')
+# -------------------------------------------------------------------------
+# Define start and end DummyOperators
+# -------------------------------------------------------------------------
+start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
 
-    create_redshift_tables = PostgresOperator(
-        task_id='Create_tables',
-        postgres_conn_id="redshift",
-        sql='common/sql_queries.sql'
-    )
+end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
 
-    stage_events_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_events',
-        table="staging_events",
-        redshift_conn_id="redshift",
-        aws_credentials_id="aws_credentials",
-        s3_bucket=s3_bucket,
-        s3_key=events_s3_key,
-        log_json_file=log_json_file
-    )
 
-    # s3_key="{events_s3_key}/{execution_date.year}/{execution_date.month}/{ds}-events.json"
+# -------------------------------------------------------------------------
+# Task: Create Tables (optional, but good for first run)
+# -------------------------------------------------------------------------
+create_tables = PostgresOperator(
+    task_id='Create_tables',
+    postgres_conn_id='redshift',
+    sql='create_tables.sql',
+    dag=dag
+)
 
-    stage_songs_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_songs',
-        table="staging_songs",
-        redshift_conn_id="redshift",
-        aws_credentials_id="aws_credentials",
-        s3_bucket=s3_bucket,
-        s3_key=songs_s3_key
-    )
+# -------------------------------------------------------------------------
+# Stage Events and Songs to Redshift
+# -------------------------------------------------------------------------
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    table='staging_events',
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    s3_bucket='airflow-project-bucket123',
+    s3_key='log-data',
+    copy_json_option='s3://udacity-dend/log_json_path.json'
+)
 
-    load_songplays_table = LoadFactOperator(
-        task_id='Load_songplays_fact_table',
-        redshift_conn_id="redshift",
-        sql_query=final_project_sql_statements.sql.songplay_table_insert
-    )
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    table='staging_songs',
+    redshift_conn_id='redshift',
+    aws_credentials_id='aws_credentials',
+    s3_bucket='uc-de-airflow-aws',
+    s3_key='song-data',
+    copy_json_option='auto'
+)
 
-    load_user_dimension_table = LoadDimensionOperator(
-        task_id='Load_user_dim_table',
-        redshift_conn_id="redshift",
-        sql_query=final_project_sql_statements.user_table_insert
-    )
+# -------------------------------------------------------------------------
+# Load Fact Table
+# -------------------------------------------------------------------------
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    redshift_conn_id='redshift',
+    table='songplays',
+    sql_query=SqlQueries.songplay_table_insert
+)
 
-    load_song_dimension_table = LoadDimensionOperator(
-        task_id='Load_song_dim_table',
-        redshift_conn_id="redshift",
-        sql_query=final_project_sql_statements.song_table_insert
-    )
-
-    load_artist_dimension_table = LoadDimensionOperator(
-        task_id='Load_artist_dim_table',
-        redshift_conn_id="redshift",
-        sql_query=final_project_sql_statements.artist_table_insert
-    )
-
-    load_time_dimension_table = LoadDimensionOperator(
-        task_id='Load_time_dim_table',
-        redshift_conn_id="redshift",
-        sql_query=final_project_sql_statements.time_table_insert
-    )
-
-    run_quality_checks = DataQualityOperator(
-        task_id='Run_data_quality_checks',
-        redshift_conn_id = "redshift",
-        tables = ["songplays", "users", "songs", "artists", "time"]
-    )
-
-    end_operator = DummyOperator(task_id='End_execution')
-
-    start_operator >> create_redshift_tables >> [stage_events_to_redshift, stage_songs_to_redshift] >> \
-    load_songplays_table >> [load_user_dimension_table, load_song_dimension_table, load_artist_dimension_table, load_time_dimension_table] >> \
-    run_quality_checks >> end_operator
-
-final_project_dag = final_project()
+# ---------
