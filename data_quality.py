604c3deb799ebfd8ce86325f74b4ed0887fcc3f7
Akshatha912 <127100110+Akshatha912@users.noreply.github.com>
2025-11-11 18:33:36 +0530

    Update data_quality.py

--- data_quality.py
+++ data_quality.py
@@ -9,41 +9,41 @@ from airflow.hooks.postgres_hook import PostgresHook
 
 
 class DataQualityOperator(BaseOperator):
-ui_color = '#89DA59'
+    ui_color = '#89DA59'
 
 
-@apply_defaults
-def __init__(self,
-redshift_conn_id='redshift',
-tests=None,
-*args, **kwargs):
-super(DataQualityOperator, self).__init__(*args, **kwargs)
-self.redshift_conn_id = redshift_conn_id
-self.tests = tests or []
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id='redshift',
+                 tests=None,
+                 *args, **kwargs):
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.tests = tests or []
 
 
-def _evaluate(self, result, expected):
-"""Evaluate result against expected which can be:
-- dict like {'gt': 0}, {'eq': 5}
-- int (equality)
-- None -> default to > 0
-"""
-if isinstance(expected, dict):
-if 'gt' in expected:
-return result > expected['gt']
-if 'eq' in expected:
-return result == expected['eq']
-if 'gte' in expected:
-return result >= expected['gte']
-if 'lt' in expected:
-return result < expected['lt']
-return False
-elif isinstance(expected, int):
-return result == expected
-else:
-# default expectation: result > 0
-return result > 0
+    def _evaluate(self, result, expected):
+        """Evaluate result against expected which can be:
+           - dict like {'gt': 0}, {'eq': 5}
+           - int (equality)
+           - None -> default to > 0
+        """
+        if isinstance(expected, dict):
+            if 'gt' in expected:
+                return result > expected['gt']
+            if 'eq' in expected:
+                return result == expected['eq']
+            if 'gte' in expected:
+                return result >= expected['gte']
+            if 'lt' in expected:
+                return result < expected['lt']
+            return False
+       elif isinstance(expected, int):
+            return result == expected
+       else:
+       # default expectation: result > 0
+            return result > 0
 
 
-def execute(self, context):
-re
+    def execute(self, context):
+        re
